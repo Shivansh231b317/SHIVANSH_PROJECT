f678777818a410a9ce895c2ccd7b6aee47680970
231B316 <231B316@juet.co.in>
2025-08-12 14:58:08 +0530

    shivam work done

---
+++ Saini.java
@@ -0,0 +1,14 @@
+class GFG {
+    public static int sum(int num1, int num2)
+    {
+        return num1+num2;
+    }
+    
+      
+    public static void main(String[] args)
+    {
+        GFG ob = new GFG();
+        int res = ob.sum(28, 49);
+        
+    }
+}
